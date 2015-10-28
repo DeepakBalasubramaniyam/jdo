68b0526170194d64c509e371f12d8c409f5efe2a
Deepak <deepak.bala@a-cti.com>
2015-10-28 15:01:31 +0530

    This works fine except the orange button

---
+++ GoogleSystemStatuswithJDO/src/com/example/myproject/GoogleSystemStatuswithJDOServlet.java
@@ -0,0 +1,91 @@
+package com.example.myproject;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.jdo.PersistenceManager;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class GoogleSystemStatuswithJDOServlet extends HttpServlet {
+	/*public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String time=request.getParameter("time");
+		String reason=request.getParameter("reason");
+		PersistenceManager pm=PMF.get().getPersistenceManager();
+		Example ex=new Example();
+		ex.setTime(time);
+		ex.setReason(reason);
+		pm.makePersistent(ex);
+		ArrayList al=new ArrayList();
+		for(Example e:pm.getExtent(Example.class,false)){
+			
+			al.add(e.getTime());
+			
+	    	
+		}
+		request.setAttribute("time",al);
+		
+		
+		ArrayList al1=new ArrayList();
+		for(Example e:pm.getExtent(Example.class,false)){
+			
+			al1.add(e.getReason());
+			
+	    	
+		}
+		
+		request.setAttribute("reason",al1);
+		request.getRequestDispatcher("successs.jsp").forward(request,response);
+		
+		
+		
+
+		pm.close();
+
+		
+		
+	}
+*/
+	
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String time=request.getParameter("time");
+		String reason=request.getParameter("reason");
+		PersistenceManager pm=PMF.get().getPersistenceManager();
+		Example ex=new Example();
+		ex.setTime(time);
+		ex.setReason(reason);
+		pm.makePersistent(ex);
+		ArrayList al=new ArrayList();
+		for(Example e:pm.getExtent(Example.class,false)){
+			
+			al.add(e.getTime());
+			
+	    	
+		}
+		request.setAttribute("time",al);
+		
+		
+		ArrayList al1=new ArrayList();
+		for(Example e:pm.getExtent(Example.class,false)){
+			
+			al1.add(e.getReason());
+			
+	    	
+		}
+		
+		request.setAttribute("reason",al1);
+		request.getRequestDispatcher("successs.jsp").forward(request,response);
+		
+		
+		
+
+		pm.close();
+
+		
+		
+	}
+
+
+
+}
---
+++ GoogleSystemStatuswithJDO/war/form.jsp
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <link href="special.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<div class="container">
+  <h2>ISSUE FORM</h2>
+  <form role="form" action="/googlesystemstatuswithjdo" method="post">
+    <div class="form-group">
+      <label>ENTER THE TIME FOR THE ISSUE:</label>
+      <input type="text" class="form-control" name="time"  placeholder="Enter the time">
+    </div>
+    <div class="form-group">
+      <label>ENTER THE REASON FOR THE ISSUE:</label>
+      <input type="text" class="form-control" name="reason"  placeholder="Enter the reason for the issue">
+    </div>
+    <button type="submit" class="btn btn-primary btn-lg btn-block">save</button>
+								
+            
+  </form>
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
